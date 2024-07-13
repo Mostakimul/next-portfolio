5908f1b088fe5dac0d847ab6efcbe38a37722447
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-07-13 10:30:33 -0400

    vercel and meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import MyHome from './(home)/page';
+import Profile from '@/components/home/Profile';
+
+export const metadata = {
+  title: "Mostakimul's Portfolio",
+  description: 'Discover About Mostakimul Karim',
+};
 
 export default function Home() {
   return (
     <main>
-      <MyHome />
+      <div className="h-full">
+        <Profile />
+      </div>
     </main>
   );
 }
